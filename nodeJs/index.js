96a8cf350d50926a83a62041abf7517e854b4be2
LiandaDuanDal <ln387408@dal.ca>
2021-06-18 22:31:49 -0300

    Node HTTP Example 2

--- nodeJs/index.js
+++ nodeJs/index.js
@@ -3,14 +3,18 @@ var rect = require('./rectangle')
 function solveRect(l,b) {
     console.log("Solving for rectangle with l = " + l + " and b = " + b);
 
-    if (l <= 0 || b <= 0) {
-        console.log("Rectangle dimensions should be greater than zero:  l = "
-               + l + ",  and b = " + b);
-    }
-    else {
-	    console.log("The area of the rectangle is " + rect.area(l,b));
-	    console.log("The perimeter of the rectangle is " + rect.perimeter(l,b));
-    }
+    rect(l,b,(err, rectangle)=>{
+        if (err){
+            console.log("ERROR:",err.message)
+        }
+        else{
+            console.log("This area of the rectangle is the rectangle of dimension "+l+
+            " and b = "+b+" is" +rectangle.area());
+            console.log("This perimeter of the rectangle is the rectangle of dimension"+l+
+            " and b = "+b+" is " +rectangle.perimeter());
+        }
+    });
+    console.log("this assignment is after the call to rect but will be executed first")
 }
 
 solveRect(2,4);
