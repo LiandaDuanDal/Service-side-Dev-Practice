7f5b4887930086050778ed4f8ad9ce3e9d924bc4
LiandaDuanDal <ln387408@dal.ca>
2021-06-19 20:54:43 -0300

    Express Generator REST API

--- nodeJs/conFusionServer/app.js
+++ nodeJs/conFusionServer/app.js
@@ -6,6 +6,11 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+// add---
+var dishRouter = require('./routes/dishRouter');
+var promoRouter = require('./routes/promoRouter');
+var leaderRouter = require('./routes/leaderRouter');
+// 
 
 var app = express();
 
@@ -18,9 +23,14 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-
+// 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+// add---
+app.use('/dishes',dishRouter);
+app.use('/promotions',promoRouter);
+app.use('/leaders',leaderRouter);
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
